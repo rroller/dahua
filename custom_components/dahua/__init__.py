68cb1bb04058720afe1bb64fd352ebc712ce7522
jays3l33t <34732936+jays3l33t@users.noreply.github.com>
2026-09-21 15:01:06 -0400

    Preserve existing source trailing whitespace

--- custom_components/dahua/__init__.py
+++ custom_components/dahua/__init__.py
@@ -2514,3 +2514,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
     await hass.config_entries.async_reload(entry.entry_id)
+
